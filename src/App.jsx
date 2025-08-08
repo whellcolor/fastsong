bbcbe3eaa9596efbf7e0ff171974ad32373818fb
gameprix <Wahyudikdl92@gmail.com>
2025-08-08 23:34:29 +0700

    Update App.jsx
    Signed-off-by: gameprix <Wahyudikdl92@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,29 @@ import Songs from "./components/Songs";
 import Likes from "./components/Likes";
 import SongDetails from "./components/SongDetails";
 import Import from "./components/Import";
+import ads from "ads.txt";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuSL5QAuV1h1r6xGBvIoYpCZsCp45ujTE",
+  authDomain: "fastsongup.firebaseapp.com",
+  projectId: "fastsongup",
+  storageBucket: "fastsongup.firebasestorage.app",
+  messagingSenderId: "4767799311",
+  appId: "1:4767799311:web:9eb1c2da3517651ee9eb7a",
+  measurementId: "G-E72PJNE1XT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 
