09e95ff3119781e8812ee56fd9129fe9e5403e72
whellcolor <bahansatekambing@gmail.com>
2026-05-06 21:47:27 +0700

    Create agaraStyles.js
    Signed-off-by: whellcolor <bahansatekambing@gmail.com>

---
+++ src/agaraStyles.js
@@ -0,0 +1,23 @@
+export const agaraStyles = {
+  body: {
+    background: "#0f172a",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "50px",
+    minHeight: "100vh",
+  },
+  box: {
+    width: "420px",
+    background: "#111827",
+    padding: "20px",
+    borderRadius: "15px",
+  },
+  button: {
+    width: "100%",
+    marginTop: "10px",
+    padding: "10px",
+    borderRadius: "10px",
+    cursor: "pointer",
+  },
+};
