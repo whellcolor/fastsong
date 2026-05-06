f71b66f21ffd3f4f6f79adbe3851100207add419
whellcolor <bahansatekambing@gmail.com>
2026-05-06 23:44:02 +0700

    Update vite.config.js
    Signed-off-by: whellcolor <bahansatekambing@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -9,3 +9,24 @@ export default defineConfig({
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
 });
+
+
+const response = await fetch("https://api.thirdweb.com/v1/transactions", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "x-secret-key": "G8vNXJOMTp_TuTQV3kgFIQyKAz_L21F3a9SW57zKanPEqK7iGCHyTUcHKuujJR1q3G2h7d4VHaIlDeR3yn0Wjw",
+  },
+  body: JSON.stringify({
+    chainId: "84532",
+    from: "3ea7634968af4a7c90b17914bcf7d4bb",
+    transactions: [
+      {
+        type: "contractCall",
+        contractAddress: "0x...",
+        method: "function mintTo(address to, uint256 amount)",
+        params: ["0x...", "100"],
+      },
+    ],
+  }),
+});
