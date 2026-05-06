43bd0c1aa4bb09dab3347b64f3038deb980264fe
whellcolor <bahansatekambing@gmail.com>
2026-05-06 21:48:29 +0700

    Create AgaraApp.jsx
    Signed-off-by: whellcolor <bahansatekambing@gmail.com>

---
+++ src/AgaraApp.jsx
@@ -0,0 +1,41 @@
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { agaraClient } from "./agaraClient";
+import { agaraMintNFT } from "./agaraMint";
+import { agaraStakeNFT } from "./agaraStake";
+import { agaraUnstakeNFT } from "./agaraUnstake";
+import { agaraStyles } from "./agaraStyles";
+
+export default function AgaraApp() {
+  const account = useActiveAccount();
+
+  return (
+    <div style={agaraStyles.body}>
+      <div style={agaraStyles.box}>
+        <h2>Agara NFT PRO</h2>
+
+        <ConnectButton client={agaraClient} />
+
+        <button
+          onClick={() => agaraMintNFT(account)}
+          style={agaraStyles.button}
+        >
+          Mint NFT
+        </button>
+
+        <button
+          onClick={() => agaraStakeNFT(account, 0)}
+          style={agaraStyles.button}
+        >
+          Stake NFT #0
+        </button>
+
+        <button
+          onClick={() => agaraUnstakeNFT(account, 0)}
+          style={agaraStyles.button}
+        >
+          Unstake NFT #0
+        </button>
+      </div>
+    </div>
+  );
+}
