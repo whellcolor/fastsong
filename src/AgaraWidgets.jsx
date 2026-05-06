6a573680a1565476d7aed553645e5c547a747fd6
whellcolor <bahansatekambing@gmail.com>
2026-05-06 22:05:42 +0700

    Update AgaraWidgets.jsx
    Signed-off-by: whellcolor <bahansatekambing@gmail.com>

--- src/AgaraWidgets.jsx
+++ src/AgaraWidgets.jsx
@@ -103,5 +103,39 @@ export default function AgaraWidgets() {
         }}
       />
     </div>
+export default function AgaraWidgets() {
+  return (
+    <div style={{ marginTop: "20px", display: "grid", gap: "20px" }}>
+      
+      <BridgeWidget client={agaraClient} theme="dark" />
+
+      <SwapWidget
+        client={agaraClient}
+        prefill={{
+          buyToken: { chainId: 8453 },
+          sellToken: { chainId: 137 },
+        }}
+      />
+
+      <CheckoutWidget
+        client={agaraClient}
+        chain={arbitrum}
+        amount="0.9"
+        seller="0xd8519A8b8825Aa0DcC73aAD572f447FAE102fe88"
+      />
+
+      <TransactionWidget
+        client={agaraClient}
+        transaction={prepareTransaction({
+          to: "0xd8519A8b8825Aa0DcC73aAD572f447FAE102fe88",
+          chain: ethereum,
+          client: agaraClient,
+          value: toUnits("0.9", 18),
+        })}
+      />
+    </div>
+  );
+}
+        
   );
 }
